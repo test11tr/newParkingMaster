7be97baf369754f87a252adad769bed48e665fb0
test11 <kaantekin67@gmail.com>
2024-01-31 01:59:28 +0300

    big progress limit problem fix

--- Assets/Game/Script/SuccessMenu.cs
+++ Assets/Game/Script/SuccessMenu.cs
@@ -11,40 +11,43 @@ namespace test11
     {
         [Header("Success Menu Manager")]
         public GameObject LoadingMenu;
-        [HideInInspector]public ParkingManager _parkingManager;
-        public string mainMenu = "00-MainMenu" ;
+        [HideInInspector] public ParkingManager _parkingManager;
+        public string mainMenu = "00-MainMenu";
         private string levelName;
-        [SerializeField] public LevelLoader _levelLoader; 
-        [SerializeField] public GameObject _continueButton; 
+        [SerializeField] public LevelLoader _levelLoader;
+        [SerializeField] public GameObject _continueButton;
         private int currentIndex;
 
-        public GameObject notEnoughVehicleLevelMenu;
         public TMP_Text buyNewCarText;
+        public GameObject notEnoughVehicleLevelMenu;
 
-        void Start ()
+
+        void Start()
         {
             levelName = SceneManager.GetActiveScene().name;
-             if (_parkingManager == null)
+            if (_parkingManager == null)
             {
                 _parkingManager = GameObject.FindGameObjectWithTag("parkingManager").GetComponent<ParkingManager>();
             }
         }
 
         // SuccessMenu continue button
-        public void Continue ()
+        public void Continue()
         {
-            LoadingMenu.SetActive(true);
-            if(PlayerPrefs.GetInt (levelName+"LevelID") >= _levelLoader.Levels.Length -1){
-                SceneManager.LoadScene(mainMenu);
-                PlayerPrefs.SetInt (levelName+"LevelID", PlayerPrefs.GetInt (levelName+"LevelID"));
-            }else{
-                if (PlayerPrefs.GetInt(levelName + "LevelID") > 14)
+            if (PlayerPrefs.GetInt(levelName + "LevelID") > 14)
+            {
+                if (PlayerPrefs.GetInt("CurrentCar") < 3)
                 {
-                    if (PlayerPrefs.GetInt("CurrentCar") < 3)
+                    buyNewCarText.text = "Your vehicle is too old. Please buy the 3rd car or better!";
+                    notEnoughVehicleLevelMenu.SetActive(true);
+                }
+                else
+                {
+                    LoadingMenu.SetActive(true);
+                    if (PlayerPrefs.GetInt(levelName + "LevelID") >= _levelLoader.Levels.Length - 1)
                     {
-                        print("imhere");
-                        buyNewCarText.text = "Your vehicle is too old. Please buy the 3rd car or better!";
-                        notEnoughVehicleLevelMenu.SetActive(true);
+                        SceneManager.LoadScene(mainMenu);
+                        PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID"));
                     }
                     else
                     {
@@ -52,12 +55,21 @@ namespace test11
                         PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID") + 1);
                     }
                 }
-                else if (PlayerPrefs.GetInt(levelName + "LevelID") > 25)
+            }
+            else if (PlayerPrefs.GetInt(levelName + "LevelID") > 25)
+            {
+                if (PlayerPrefs.GetInt("CurrentCar") < 4)
                 {
-                    if (PlayerPrefs.GetInt("CurrentCar") < 4)
+                    buyNewCarText.text = "Your vehicle is too old. Please buy the 4th car or better!";
+                    notEnoughVehicleLevelMenu.SetActive(true);
+                }
+                else
+                {
+                    LoadingMenu.SetActive(true);
+                    if (PlayerPrefs.GetInt(levelName + "LevelID") >= _levelLoader.Levels.Length - 1)
                     {
-                        buyNewCarText.text = "Your vehicle is too old. Please buy the 4th car or better!";
-                        notEnoughVehicleLevelMenu.SetActive(true);
+                        SceneManager.LoadScene(mainMenu);
+                        PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID"));
                     }
                     else
                     {
@@ -65,12 +77,21 @@ namespace test11
                         PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID") + 1);
                     }
                 }
-                else if (PlayerPrefs.GetInt(levelName + "LevelID") > 35)
+            }
+            else if (PlayerPrefs.GetInt(levelName + "LevelID") > 35)
+            {
+                if (PlayerPrefs.GetInt("CurrentCar") < 5)
                 {
-                    if (PlayerPrefs.GetInt("CurrentCar") < 5)
+                    buyNewCarText.text = "Your vehicle is too old. Please buy the 5th car or better!";
+                    notEnoughVehicleLevelMenu.SetActive(true);
+                }
+                else
+                {
+                    LoadingMenu.SetActive(true);
+                    if (PlayerPrefs.GetInt(levelName + "LevelID") >= _levelLoader.Levels.Length - 1)
                     {
-                        buyNewCarText.text = "Your vehicle is too old. Please buy the 5th car or better!";
-                        notEnoughVehicleLevelMenu.SetActive(true);
+                        SceneManager.LoadScene(mainMenu);
+                        PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID"));
                     }
                     else
                     {
@@ -78,35 +99,52 @@ namespace test11
                         PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID") + 1);
                     }
                 }
-                else if (PlayerPrefs.GetInt(levelName + "LevelID") > 45)
+            }
+            else if (PlayerPrefs.GetInt(levelName + "LevelID") > 45)
+            {
+                if (PlayerPrefs.GetInt("CurrentCar") < 6)
                 {
-                    if (PlayerPrefs.GetInt("CurrentCar") < 6)
+                    buyNewCarText.text = "Your vehicle is too old. Please buy the 6th car or better!";
+                    notEnoughVehicleLevelMenu.SetActive(true);
+                }
+                else
+                {
+                    LoadingMenu.SetActive(true);
+                    if (PlayerPrefs.GetInt(levelName + "LevelID") >= _levelLoader.Levels.Length - 1)
                     {
-                        buyNewCarText.text = "Your vehicle is too old. Please buy the 6th car or better!";
-                        notEnoughVehicleLevelMenu.SetActive(true);
+                        SceneManager.LoadScene(mainMenu);
+                        PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID"));
                     }
                     else
                     {
                         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                         PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID") + 1);
                     }
+                }
+            }
+            else
+            {
+                LoadingMenu.SetActive(true);
+                if (PlayerPrefs.GetInt(levelName + "LevelID") >= _levelLoader.Levels.Length - 1)
+                {
+                    SceneManager.LoadScene(mainMenu);
+                    PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID"));
                 }
                 else
                 {
                     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
                     PlayerPrefs.SetInt(levelName + "LevelID", PlayerPrefs.GetInt(levelName + "LevelID") + 1);
                 }
-                
             }
         }
 
-        public void Retry ()
+        public void Retry()
         {
             LoadingMenu.SetActive(true);
-            SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
-        public void Exit ()
+        public void Exit()
         {
             LoadingMenu.SetActive(true);
             SceneManager.LoadScene(mainMenu);
